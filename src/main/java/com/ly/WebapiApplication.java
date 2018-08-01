54ddee0e291b2a68df6b74246689e168cdbd696e
1004789224 <32547368+1004789224@users.noreply.github.com>
2018-08-01 15:24:17 +0800

    仪器的增删查改 测试

--- src/main/java/com/ly/WebapiApplication.java
+++ src/main/java/com/ly/WebapiApplication.java
@@ -2,9 +2,11 @@ package com.ly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //@EnableDiscoveryClient
 @SpringBootApplication
+@EnableTransactionManagement
 public class WebapiApplication {
 
     public static void main(String[] args) {
